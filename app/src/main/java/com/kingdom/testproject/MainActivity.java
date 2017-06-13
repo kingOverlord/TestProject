67eeecd94704dc408d6b21fd10ef8febd73a70bd
KungFu <liankobe>
2017-06-13 16:40:27 +0800

    test

--- app/src/main/java/com/kingdom/testproject/MainActivity.java
+++ app/src/main/java/com/kingdom/testproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.kingdom.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View view=new View(this);
+        Log.i("kkk", "kkkk");
     }
 }
